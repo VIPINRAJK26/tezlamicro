46ddf0a96f2b835b4b3d0f53e62b5777f805bd6b
VIPINRAJK26 <vipinrajk026@gmail.com>
2024-10-16 17:40:56 +0530

    navbar update

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const CustomNavbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const navbarRef = useRef(null);
+
   const categories = [
     { name: "Tubular Batteries", path: "Batteries" },
     { name: "Inverter/Home UPS", path: "Home Ups" },
@@ -12,14 +15,39 @@ const CustomNavbar = () => {
     { name: "MPPT'S", path: "MPPT" },
   ];
 
+  // Close navbar when clicking outside of it
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+        setNavbarOpen(false);
+      }
+    };
+
+    if (navbarOpen) {
+      document.addEventListener("click", handleOutsideClick);
+    } else {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [navbarOpen]);
+
   return (
-    <Navbar expand="lg" className="custom-navbar" sticky="top">
+    <Navbar
+      expand="lg"
+      className="custom-navbar"
+      sticky="top"
+      ref={navbarRef}
+      expanded={navbarOpen}
+    >
       <Container>
         {/* Brand logo */}
         <Navbar.Brand>
           <Link to="/">
             <img
-              src="\logo-transparant.png"
+              src="/logo-transparant.png"
               alt="Logo"
               className="navbar-logo"
             />
@@ -27,7 +55,10 @@ const CustomNavbar = () => {
         </Navbar.Brand>
 
         {/* Hamburger toggle for mobile */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setNavbarOpen(!navbarOpen)}
+        />
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="nav-main">
@@ -37,12 +68,17 @@ const CustomNavbar = () => {
               </Link>
             </Nav.Link>
 
-            <NavDropdown title="PRODUCTS" className="text-black" id="home-solutions-dropdown">
+            <NavDropdown
+              title="PRODUCTS"
+              className="text-black"
+              id="home-solutions-dropdown"
+            >
               {categories.map((category, index) => (
                 <NavDropdown.Item key={index}>
                   <Link
-                    to={`/preview/${category.path}`} 
+                    to={`/preview/${category.path}`}
                     className="text-decoration-none text-black"
+                    onClick={() => setNavbarOpen(false)} // Close navbar on link click
                   >
                     {category.name}
                   </Link>
@@ -51,12 +87,20 @@ const CustomNavbar = () => {
             </NavDropdown>
 
             <Nav.Link>
-              <Link to="/dealer" className="text-decoration-none text-black nav-item">
+              <Link
+                to="/dealer"
+                className="text-decoration-none text-black nav-item"
+                onClick={() => setNavbarOpen(false)} // Close navbar on link click
+              >
                 DEALER LOCATOR
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link to="/contact" className="text-decoration-none text-black nav-item">
+              <Link
+                to="/contact"
+                className="text-decoration-none text-black nav-item"
+                onClick={() => setNavbarOpen(false)} // Close navbar on link click
+              >
                 CONTACT
               </Link>
             </Nav.Link>
